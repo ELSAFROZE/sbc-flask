e6dbc5379c560d8fae7e58d1b0c97358ade5464f
safalbk333@gmail.com <safalbk333@gmail.com>
2019-02-23 14:55:15 +0530

    html type also included

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ app=Flask(__name__)
 
 @app.route("/")
 def india():
-    return "i love india !!!!"
+    return "<h1> i love india hi !!!! <h1>"
+
 @app.route("/home")
 def homey():
     return "im from kottayam"   
+
 @app.route("/contact")
 def contct():
     return"this is my contact :)"
 
 if(__name__=="__main__"):
-    app.run()
+    app.run(debug = True)
